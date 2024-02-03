1506fc6b08dc122cfabc4de6e76f16f390d6e1ad
Eduard Krasnoshock <powered76@gmail.com>
2024-02-04 00:54:58 +0300

    4.02.24 commit

--- gulp-start/app/js/main.js
+++ gulp-start/app/js/main.js
@@ -1,5 +1,9 @@
 $(function(){
-
+    
+$('.products__slider').slick({
+    prevArrow:  '<button class="slider-btn slider-btn__left"><img src="images/arrow-left.svg" alt=""></button>',
+  nextArrow: '<button class="slider-btn slider-btn__right"><img src="images/arrow-right.svg" alt=""></button>'
+});
 
 
   
