977a39a54b403f5af87ccb33047a6b1dc30da701
Eduard Krasnoshock <powered76@gmail.com>
2024-02-08 02:01:21 +0300

    07.02.24 fix commit

--- gulp-start/app/js/main.js
+++ gulp-start/app/js/main.js
@@ -6,6 +6,12 @@ $(function () {
     infinite: false,
   });
 
+$('.questions__item-title').on('click', function(){
+  $('.questions__item').removeClass('questions__item--active');
+  $(this).parent().addClass('questions__item--active');
+
+})
+
 
 
 });
